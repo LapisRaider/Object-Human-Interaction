94d2878f83fbd3b2932795bba54341a7b8e2dde1
Angie <angielimenqi7@gmail.com>
2024-03-04 06:43:37 +0800

    Object tracking via YOLO

--- environment.sh
+++ environment.sh
@@ -3,7 +3,7 @@ ENV_NAME="Obj_Human_Interaction"
 conda create --name ${ENV_NAME} python=3.8
 conda activate ${ENV_NAME}
 
-conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=10.2 -c pytorch
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch -c nvidia
 # pip install --upgrade setuptools
 # pip install numpy==1.25.2
 # pip install opencv-python==4.8.0.76
--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 import yaml
 import os
 import argparse
+import torch
 from ultralytics import YOLO
 
 yoloModel = None
 availableObjs = None
+yoloClassNameIndexMap = None
+
 
 def main(_videoPath):
-    print("hello world")
+    print("hello")
+
+def loadYolo(_yoloParams):
+    yoloModel = YOLO(_yoloParams["checkpoint_file"])
+
+    with open(_yoloParams["dataset_file"], 'r', encoding='utf-8') as file:
+        yoloDataSetConfigs = yaml.safe_load(file)
+
+    yoloClassNameIndexMap = {name: idx for idx, name in yoloDataSetConfigs["names"].items()}
+
+    classesIdToDetect = [yoloClassNameIndexMap.get(key) for key in availableObjs.keys() if key in yoloClassNameIndexMap]
+    classesIdToDetect.append(yoloClassNameIndexMap.get('person'))
+
+    results = yoloModel.track(source="https://youtu.be/UeiNdPaQ1IA?si=U3WrVcY4OXfPdnVt", 
+                              show=True, 
+                              conf=_yoloParams["confidence_score"],
+                              iou=_yoloParams["intersection_over_union"],
+                              device=_yoloParams["device"],
+                              classes=classesIdToDetect)
+
 
 def drawBoundary():
     print("DRAW")
@@ -23,9 +45,8 @@ if __name__ == "__main__":
     with open(arguments.config) as f:
         config = yaml.safe_load(f)
 
-    yoloModel = YOLO(config["yolo_params"]["checkpoint_file"])
     availableObjs = config.get("interactable_objs", {})
+    
 
-    results = yoloModel.track(source="https://youtu.be/UeiNdPaQ1IA?si=U3WrVcY4OXfPdnVt", show=True)
-
+    loadYolo(config["yolo_params"])
     main(arguments.input)
